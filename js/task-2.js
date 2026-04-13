b2db4745fbbb2d767654829934a881b35fcb08ee
Bohdana Poliuchovych <ddana2348@gmail.com>
2026-04-13 14:36:12 +0200

    add task-1,task-2

--- js/task-2.js
+++ js/task-2.js
@@ -24,3 +24,14 @@ const images = [
     alt: "Lighthouse Coast Sea",
   }
 ];
+
+
+const galleryList = document.querySelector(".gallery");
+
+const markup = images
+  .map(image => `
+    <li class="gallery-item">
+      <img class="gallery-img" src="${image.url}" alt="${image.alt}">
+    </li>`)
+
+galleryList.insertAdjacentHTML("beforeend", markup);
